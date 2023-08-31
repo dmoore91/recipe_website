1bdd5e2369eda6c58935f633c309c89c81acc7e8
Dan <danmoore2706@gmail.com>
2023-08-30 20:42:54 -0400

    Init DB

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask           # import flask
+
+from models import Schema
+
 app = Flask(__name__)             # create an app instance
+
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
     return "Hello World!"         # which returns "hello world"
+
+
 if __name__ == "__main__":        # on running python app.py
+    Schema()
     app.run()                     # run the flask app
--- models.py
+++ models.py
@@ -3,28 +3,97 @@ import sqlite3
 
 class Schema:
     def __init__(self):
-        self.conn = sqlite3.connect('recipes.db')
-        self.create_to_do_table()
-        # Why are we calling user table before to_do table
-        # what happens if we swap them?
+        self.conn = sqlite3.connect('recipe_app.db')
+        self.create_recipes_table()
+        self.create_categories_table()
+        self.create_ingredient_categories_table()
+        self.create_units_table()
+        self.create_ingredient_table()
+        self.conn.commit()
+        self.create_recipe_to_categories_table()
+        self.create_ingredient_to_categories_table()
 
-    def create_to_do_table(self):
+    def create_recipes_table(self):
 
         query = """
-        CREATE TABLE IF NOT EXISTS "Recipes " (
-          id INTEGER PRIMARY KEY,
-          Title TEXT,
-          Description TEXT,
-          _is_done boolean,
-          _is_deleted boolean,
-          CreatedOn Date DEFAULT CURRENT_DATE,
-          DueDate Date,
-          UserId INTEGER FOREIGNKEY REFERENCES User(_id)
+        CREATE TABLE IF NOT EXISTS "Recipes" (
+          recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+          recipe_name TEXT,
+          link TEXT,
+          instructions TEXT
         );
         """
 
         self.conn.execute(query)
-    def create_user_table(self):
-        # create user table in similar fashion
-        # come on give it a try it's okay if you make mistakes
-        pass
+
+    def create_categories_table(self):
+
+        query = """
+        CREATE TABLE IF NOT EXISTS "Categories" (
+          category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+          category_name TEXT
+        );
+        """
+
+        self.conn.execute(query)
+
+    def create_ingredient_categories_table(self):
+
+        query = """
+        CREATE TABLE IF NOT EXISTS "Ingredient_Categories" (
+          ingredient_category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+          ingredient_category_name TEXT
+        );
+        """
+
+        self.conn.execute(query)
+
+    def create_units_table(self):
+
+        query = """
+        CREATE TABLE IF NOT EXISTS "Units" (
+          unit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+          unit TEXT
+        );
+        """
+
+        self.conn.execute(query)
+
+    def create_ingredient_table(self):
+
+        query = """
+        CREATE TABLE IF NOT EXISTS "Ingredients" (
+          ingredient_id INTEGER PRIMARY KEY AUTOINCREMENT,
+          ingredient_name TEXT,
+          ingredient_unit_id INTEGER,
+          FOREIGN KEY(ingredient_unit_id) REFERENCES Units(unit_id) 
+        );
+        """
+
+        self.conn.execute(query)
+
+    def create_recipe_to_categories_table(self):
+
+        query = """
+        CREATE TABLE IF NOT EXISTS "Recipe_to_Categories" (
+          recipe_id INTEGER,
+          category_id INTEGER,
+          FOREIGN KEY(category_id) REFERENCES Categories(category_id), 
+          FOREIGN KEY(recipe_id) REFERENCES Recipes(recipe_id) 
+        );
+        """
+
+        self.conn.execute(query)
+
+    def create_ingredient_to_categories_table(self):
+
+        query = """
+        CREATE TABLE IF NOT EXISTS "Ingredient_to_Categories" (
+          ingredient_id INTEGER,
+          ingredient_category_id INTEGER,
+          FOREIGN KEY(ingredient_id) REFERENCES Ingredients(ingredient_id),
+          FOREIGN KEY(ingredient_category_id) REFERENCES Ingredient_Categories(ingredient_category_id) 
+        );
+        """
+
+        self.conn.execute(query)
